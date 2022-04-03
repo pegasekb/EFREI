385348643af36ae2b1a926f8b18426247b038f5d
Paul <Paul@bg.fr>
2022-04-03 12:37:53 +0200

    .

--- Powershell_Scripting/4.ps1
+++ Powershell_Scripting/4.ps1
@@ -12,20 +12,56 @@
 
 # d.Le script propose un nom et un mot de passe alphanumérique à l’utilisateur
 
+$test = 0
+for ($b = 0; $b -lt 5; $b++) {
+
+
 $Login = Read-Host "Login" 
-$pass = Read-Host "Password" -AsSecureString 
+$Pass = Read-Host "Password" #-AsSecureString 
+Write-Host $Pass
 $nbline = (Get-content "user.txt").Length
 
 
+
+    
+
 For($i=0;$i -lt $nbline;$i++)
 {
-    $ligne  = (Get-content ("user.txt"))[$i]
-    if ($ligne -eq $Login){
-        Write-Host $ligne
+    $lignelog  = (Get-content ("user.txt"))[$i]
+    if ($lignelog -eq $Login){
+        $test = 1
+        $lignepass  = (Get-content ("pass.txt"))[$i]
+        $Dateheure= get-date
+        if ($lignepass -eq $Pass) {
+            Write-Host "Vous etes authentifie"
+            Add-Content -Path "$Login.txt" -Value "Connection reussi sur le compte $Login à $Dateheure"
+        }
+        else {
+            Write-Host "Mauvais login ou password"
+            Add-Content -Path "$Login.txt" -Value "Connection échoué sur le compte $Login à $Dateheure"
+        }
     }
-
+    elseif ($lingelog -gt $login) {
+        break
+    }
+    
 }
-# Write-Host $Login $pass
-Write-Host $nbline
+}
+Write-Host "Trop de connection avec echec"
 
+if ($test -eq 0) {
+Write-Host "Si vous voulez vous enregister voici votre login et password"
+
+
+$Newpass = -join (1..20 | ForEach {[char]((97..122) + (48..57) | Get-Random)})
+Write-Host $Login
+Write-Host $Newpass
+$reponse = Read-Host "Ce mdp vous convient ? O / N"
+if ($reponse -eq "O")  {
+    Add-Content -Path "user.txt" -Value $Login
+    Add-Content -Path "pass.txt" -Value $Newpass
+    Write-Host "Les infos sont enregistrer"
+    
+}
+}
 
--- Powershell_Scripting/5.ps1
+++ Powershell_Scripting/5.ps1
@@ -1,7 +1,32 @@
 #Paul Blanc
 
 # Créer un script qui prend deux répertoirescomme paramètres
-# a.Le script vérifie que les deux répertoires existentpuis retourne un fichier «.dat» 
+# a.Le script vérifie que les deux répertoires existent puis retourne un fichier «.dat» 
 # contenant les fichiers partagés et non partagés entre les deux répertoires.
 
 
+
+$dossier1 = Read-Host "Dossier 1"
+$dossier2 = Read-Host "Dossier 2"
+
+$dir1 = Get-ChildItem -Path $dossier1 -Name
+$dir2 = Get-ChildItem -Path $dossier2 -Name
+# Write-Host $dir1
+# Write-Host $dir2
+
+for ($linedir1 = 0; $linedir1 -lt $dir1.Count; $linedir1++) {
+    # write-host " "
+    # Write-Host $dir1[$linedir1]
+
+    # Write-Host $linedir1
+
+    for ($linedir2 = 0; $linedir2 -lt $dir2.Count; $linedir2++) {
+        # Write-Host $dir2[$linedir2]
+        # Write-Host $linedir2
+        if ($dir1[$linedir1] -eq $dir2[$linedir2]) {
+            Add-Content -Path "filecommun.dat" -Value ($dir1[$linedir1], "est dans les 2 repertoir"       )
+        }
+    }
+
+
+}
